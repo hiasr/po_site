4bb70988587df58bfecbd34339e785fc94985f91
Ruben Hias <ruben.hias@gmail.com>
2020-03-10 20:56:48 +0100

    Added html and css

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == "POST" and request.form['pass'] == "pass":
+        return redirect(url_for("buy"))
     return render_template("home.html")
 
 
 @app.route("/buy")
 def buy():
-    return "This is the buying page"
+    return render_template("buy.html")
 
 
 if __name__ == "__main__":
