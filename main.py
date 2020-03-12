d1c645ad8f18178c63b037c176eb1a67f7ef027f
Ruben Hias <ruben.hias@gmail.com>
2020-03-12 11:35:28 +0100

    Added to gcloud and socket

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+import threading
+import socket
+import time
 
 app = Flask(__name__)
 
@@ -10,10 +13,28 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/buy")
+@app.route("/buy", methods=["POST", "GET"])
 def buy():
+    if request.method == "POST":
+        pass
     return render_template("buy.html")
 
 
+def server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("192.168.0.144", 9999))
+        while 1:
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected')
+                time.sleep(10)
+                conn.sendall("YEET")
+                s.close()
+                break
+
+
 if __name__ == "__main__":
+    x = threading.Thread(target=server, daemon=True)
+    x.start()
     app.run(debug=True)
